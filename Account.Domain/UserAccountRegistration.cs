579c27adf3e15b1563a3438389b96eb613f8fb85
Régis MARTEAU <regis.marteau@agicap.fr>
2021-05-18 14:00:56 +0200

    DomainService creation

---
+++ Account.Domain/UserAccountRegistration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Account.Domain
+{
+    public class UserAccountRegistration
+    {
+        private readonly IUserAccountRepository userAccountRepository;
+
+        public UserAccountRegistration(IUserAccountRepository userAccountRepository)
+        {
+            this.userAccountRepository = userAccountRepository;
+        }
+
+
+        public async Task Register(Guid id, string email, string password)
+        {
+            UserAccount userAccount = new UserAccount(id, email, password);
+            await this.userAccountRepository.Save(userAccount);
+        }
+    }
+}
